43149619d5c26c73e3639ab4adcf7364240d0bf7
rz <muhamaul@gmail.com>
2023-08-28 14:50:11 +0700

    finished chapter 5

--- ch05/hello-buffer-config.h
+++ ch05/hello-buffer-config.h
@@ -1,8 +1,8 @@
 struct data_t
 {
-  int pid;
-  int uid;
-  char command[16];
-  char message[12];
-  char path[16];
+   int pid;
+   int uid;
+   char command[16];
+   char message[12];
+   char path[16];
 };
